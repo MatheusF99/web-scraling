68bf77469b55d1a27b04a346c30a185b5f55d87f
matheusF99 <mc79846@gmail.com>
2021-01-12 13:41:08 -0300

    capturando os dados do site

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Tecnologias:
     - JavaScript
     - PuppeteerJS
+
+ideias, criar uma variavel que va pela url e direcione para pagina do perfil no instagram
+
+cheeriosJS - outra alternativa
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://rocketseat.com.br');
-  await page.screenshot({path: 'rocketseat.png'});
+  await page.goto('https://www.instagram.com/jefftewf');
+  // await page.screenshot({path: 'rocketseat.png'});
 
-  await browser.close();
+  const imgList = await page.evaluate(() => {
+      // pegar as img que estao na parte dos posts
+        const nodeList = document.querySelectorAll('article img')
+      //tranformar o nodeList em array
+        const imgArray = [...nodeList]
+      // transforamar os elemntos html em elemntos js
+        const imgList = imgArray.map(img =>(
+          {
+            src: img.src
+          }
+        ))
+      // tirar de dentro da funcao
+      return imgList
+  })
+
+  // await browser.close();
 })(); 
